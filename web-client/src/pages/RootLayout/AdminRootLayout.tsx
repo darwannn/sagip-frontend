984306c9eb3e0b7711e48a6c3c1a43042b3bedf2
Clarence Joseph Dimafelix <clarencejoseph.dimafelix@gmail.com>
2023-06-07 09:44:04 +0800

    add temporary navigation

--- web-client/src/pages/RootLayout/AdminRootLayout.tsx
+++ web-client/src/pages/RootLayout/AdminRootLayout.tsx
@@ -1,9 +1,43 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 const AdminRootLayout = () => {
   return (
     <>
       {/* <h1>Admin Root Layout</h1> */}
+      <nav>
+        <ul>
+          <li>
+            <NavLink to={"/"}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/users"}>Users</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/teams"}>Teams</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/emergency-reports"}>Emergency Reports</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/hazard-reports"}>Hazard Reports</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/facility-map"}>Facility Map</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/disaster-alerts"}>Disaster Alerts</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/login"}>Login</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/register"}>Register</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/forgot-password"}>Forgot Password</NavLink>
+          </li>
+        </ul>
+      </nav>
       <Outlet />
     </>
   );
