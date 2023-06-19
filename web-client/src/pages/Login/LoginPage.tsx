368ee574c95a5edd66223a7cd26b9a2654fe351f
Clarence Joseph Dimafelix <clarencejoseph.dimafelix@gmail.com>
2023-06-19 13:02:54 +0800

    feat: display input errors

--- web-client/src/pages/Login/LoginPage.tsx
+++ web-client/src/pages/Login/LoginPage.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react";
-import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
-
-// REDUX
-import { useAppDispatch } from "../../store/hooks";
-import { login } from "../../store/slices/authSlice";
+import { useState } from "react";
+import {
+  ActionFunction,
+  Form,
+  redirect,
+  useActionData,
+  useNavigation,
+} from "react-router-dom";
 
 // Types
 import { AuthResponse } from "../../types/auth";
@@ -12,69 +14,28 @@ import { API_BASE_URL } from "../../api.config";
 import { setAuthToken } from "../../util/auth";
 
 const LoginPage = () => {
-  // Redux
-  const dispatch = useAppDispatch();
-
   const [identifier, setIdentifier] = useState("");
   const [password, setPassword] = useState("");
   // const [isLoading, setIsLoading] = useState(false);
 
-  const actionData = useActionData();
+  /**
+   * This is a temporary workaround for the issue
+   * as useActionData / useLoaderData currently doesnt support generic types.
+   * MORE INFO: https://github.com/remix-run/react-router/discussions/9792
+   */
+  const actionData = useActionData() as AuthResponse;
+
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
-
-  // const onSubmitFormHandler = async (
-  //   event: React.FormEvent<HTMLFormElement>
-  // ) => {
-  //   event.preventDefault();
-  //   try {
-  //     setIsLoading(true);
-  //     const res = await fetch(`${API_BASE_URL}/auth/login`, {
-  //       method: "POST",
-  //       body: JSON.stringify({
-  //         identifier: username,
-  //         password: password,
-  //       }),
-  //       headers: { "Content-Type": "application/json" },
-  //     });
-
-  //     if (!res.ok) {
-  //       const errorData = await res.json();
-  //       console.log(errorData);
-  //       throw new Error(errorData.message);
-  //     }
-  //     const data = await res.json();
-  //     console.log(data);
-  //     dispatch(
-  //       login({
-  //         token: data.token,
-  //         user: {
-  //           for: data.user.for,
-  //           id: data.user.id,
-  //           status: data.user.status,
-  //           userType: data.user.userType,
-  //         },
-  //       })
-  //     );
-  //     setIsLoading(false);
-  //   } catch (error) {
-  //     setIsLoading(false);
-  //     console.error(error);
-  //     // Handle error here, such as displaying an error message to the user
-  //   }
-  //   /**
-  //    * TODO: Redirect user after logging in
-  //    * - redirect('/') is not working here
-  //    * I think it only works on 'loader' and 'action' functions
-  //    * of RRDv6
-  //    */
-  // };
 
   return (
     <>
       <h1>Login Page</h1>
       <Form method="POST" className="flex flex-col w-min gap-2">
         <label htmlFor="identifier">Identifier:</label>
+        {actionData && actionData.identifier && (
+          <span>{actionData.identifier}</span>
+        )}
         <input
           name="identifier"
           id="identifier"
@@ -84,6 +45,9 @@ const LoginPage = () => {
           onChange={(e) => setIdentifier(e.target.value)}
         />
         <label htmlFor="password">Password:</label>
+        {actionData && actionData.password && (
+          <span>{actionData.password}</span>
+        )}
         <input
           name="password"
           id="password"
@@ -109,8 +73,12 @@ export default LoginPage;
 /**
  * Login Action
  */
-
-export const loginAction = async ({ request }: { request: Request }) => {
+// eslint-disable-next-line react-refresh/only-export-components
+export const loginAction: ActionFunction = async ({
+  request,
+}: {
+  request: Request;
+}) => {
   const data = await request.formData();
   const loginData = {
     identifier: data.get("identifier"),
