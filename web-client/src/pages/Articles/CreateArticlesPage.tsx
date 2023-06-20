72539820631c61f48304d6c7f2e3e068f892bb0a
Clarence Joseph Dimafelix <clarencejoseph.dimafelix@gmail.com>
2023-06-20 20:13:02 +0800

    add: file dropzone component and onchange state handling

--- web-client/src/pages/Articles/CreateArticlesPage.tsx
+++ web-client/src/pages/Articles/CreateArticlesPage.tsx
@@ -3,10 +3,25 @@ import Tiptap from "../../components/tiptap-text-editor/Tiptap";
 import StarterKit from "@tiptap/starter-kit";
 import CustomMenuBar from "../../components/tiptap-text-editor/CustomMenuBar";
 import { useState } from "react";
+import FileDropzone from "../../components/Form/FileDropzone";
 
 const CreateArticlesPage = () => {
   const [title, setTitle] = useState<string>("");
+  const [imageSrc, setImageSrc] = useState<string>("");
+  const [imageName, setImageName] = useState<string | undefined>("");
   const [content, setContent] = useState<string>("");
+
+  const onFileChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    setImageName(file?.name);
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = (e) => {
+        setImageSrc(e.target?.result as string);
+      };
+    }
+  };
 
   const editor = useEditor({
     extensions: [StarterKit],
@@ -19,38 +34,14 @@ const CreateArticlesPage = () => {
   return (
     <div className="m-5">
       <h1>Create Articles Page</h1>
-      <div className="flex items-center justify-center w-full">
-        <label
-          htmlFor="dropzone-file"
-          className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 ligth:hover:bg-bray-800 light:bg-gray-700 hover:bg-gray-100 light:border-gray-600 light:hover:border-gray-500 light:hover:bg-gray-600"
-        >
-          <div className="flex flex-col items-center justify-center pt-5 pb-6">
-            <svg
-              aria-hidden="true"
-              className="w-10 h-10 mb-3 text-gray-400"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-              ></path>
-            </svg>
-            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-              <span className="font-semibold">Click to upload</span> or drag and
-              drop
-            </p>
-            <p className="text-xs text-gray-500 dark:text-gray-400">
-              SVG, PNG, JPG or GIF (MAX. 800x400px)
-            </p>
-          </div>
-          <input id="dropzone-file" type="file" className="hidden" />
-        </label>
-      </div>
+      {imageName && imageSrc ? (
+        <div>
+          <img src={imageSrc} alt="preview" />
+          <p>{`Image Name: ${imageName}`}</p>
+        </div>
+      ) : (
+        <FileDropzone handleFileChange={onFileChangeHandler} />
+      )}
       <div className="articleDetails w-full">
         <label htmlFor="articleTitle">Title: </label>
         <input type="text" id="articleTitle" placeholder="Title of article" />
