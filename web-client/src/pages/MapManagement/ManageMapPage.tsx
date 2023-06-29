d4dc6bd898a881c91ebb0a04bde9e8b5be5eaa0d
Clarence Joseph Dimafelix <clarencejoseph.dimafelix@gmail.com>
2023-06-29 11:38:35 +0800

    feat: display map

--- web-client/src/pages/MapManagement/ManageMapPage.tsx
+++ web-client/src/pages/MapManagement/ManageMapPage.tsx
@@ -1,7 +1,26 @@
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { GOOGLE_MAP_API_KEY } from "../../api.config";
+
+const containerStyle = {
+  width: "100vw",
+  height: "700px",
+};
+// ,
+const center = {
+  lat: 14.860767193574064,
+  lng: 120.81013409214616,
+};
+
 const ManageMapPage = () => {
   return (
     <>
       <h1>Manage Map Page</h1>
+      <LoadScript googleMapsApiKey={GOOGLE_MAP_API_KEY}>
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+          {/* Child components, such as markers, info windows, etc. */}
+          <></>
+        </GoogleMap>
+      </LoadScript>
     </>
   );
 };
