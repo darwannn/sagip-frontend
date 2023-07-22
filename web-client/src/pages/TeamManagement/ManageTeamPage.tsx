b3e58934006d0a9f6ab62f0dfd655c20157d1e48
Clarence Joseph Dimafelix <clarencejoseph.dimafelix@gmail.com>
2023-07-22 21:51:22 +0800

    feat: dynamic new team form

--- web-client/src/pages/TeamManagement/ManageTeamPage.tsx
+++ web-client/src/pages/TeamManagement/ManageTeamPage.tsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import CreateTeamForm from "./Components/CreateTeamForm";
 import TeamList from "./Components/TeamList";
 
 const ManageTeamPage = () => {
+  const [isCreateTeam, setIsCreateTeam] = useState(false);
+
   return (
     <div>
       <h1>Manage Team Page</h1>
-      <CreateTeamForm />
+      {isCreateTeam ? (
+        <CreateTeamForm />
+      ) : (
+        <button
+          className="border-2 px-2 py-1 rounded hover:bg-indigo-500 hover:text-white hover:border-indigo-500 transition-all duration-200"
+          onClick={() => setIsCreateTeam(true)}
+        >
+          Create Team
+        </button>
+      )}
       <TeamList />
     </div>
   );
