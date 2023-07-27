ff4a1f9ca1d030ea289106b7970b0c54f97fe961
Clarence Joseph Dimafelix <clarencejoseph.dimafelix@gmail.com>
2023-07-27 13:58:40 +0800

    add: modal component

--- web-client/src/pages/TeamManagement/Components/TeamDetails.tsx
+++ web-client/src/pages/TeamManagement/Components/TeamDetails.tsx
@@ -5,9 +5,11 @@ import { useLazyGetRespondersQuery } from "../../../services/responderQuery";
 import { useState } from "react";
 import { User } from "../../../types/user";
 import MembersTable from "./TeamMembersTable";
+import Modal from "../../../components/Modal/Modal";
 
 const TeamDetails = () => {
   const { id } = useParams();
+  const [showModal, setShowModal] = useState(false);
   const [isAddMode, setIsAddMode] = useState(false);
   const [selectedHead, setSelectedHead] = useState<User | null>(null); //
   const {
@@ -33,79 +35,78 @@ const TeamDetails = () => {
   }
 
   return (
-    <div className="p-5 flex-grow">
-      <div>
-        <span className="text-sm text-gray-400">{teamData?._id}</span>
-        <h1 className="text-3xl font-bold">{teamData?.name}</h1>
-      </div>
-      {/* Team Leader */}
-      <div className="relative">
-        <span>Team Leader: </span>
-        {teamData && teamData.head !== null ? (
-          <UserCard user={teamData.head} />
-        ) : (
-          <>
-            {selectedHead ? (
-              <UserCard user={selectedHead} />
-            ) : (
-              <div
-                className="border px-3 py-1 cursor-pointer rounded-md relative"
-                onClick={() => {
-                  if (!isAddMode) {
-                    setIsAddMode(true);
-                    getResponders();
-                  } else setIsAddMode(false);
-                }}
-              >
-                <p className="text-gray-700 text-[16px] leading-tight">
-                  No leader assigned for this team.
-                </p>
-                <span className="text-sm text-gray-500">
-                  Click here to assign.
-                </span>
-              </div>
-            )}
+    <>
+      <div className="p-5 flex-grow">
+        <div>
+          <span className="text-sm text-gray-400">{teamData?._id}</span>
+          <h1 className="text-3xl font-bold">{teamData?.name}</h1>
+        </div>
+        {/* Team Leader */}
+        <div className="relative">
+          <span>Team Leader: </span>
+          {teamData && teamData.head !== null ? (
+            <UserCard user={teamData.head} />
+          ) : (
+            <>
+              {selectedHead ? (
+                <UserCard user={selectedHead} />
+              ) : (
+                <div
+                  className="border px-3 py-1 cursor-pointer rounded-md relative"
+                  onClick={() => {
+                    if (!isAddMode) {
+                      setIsAddMode(true);
+                      getResponders();
+                    } else setIsAddMode(false);
+                  }}
+                >
+                  <p className="text-gray-700 text-[16px] leading-tight">
+                    No leader assigned for this team.
+                  </p>
+                  <span className="text-sm text-gray-500">
+                    Click here to assign.
+                  </span>
+                </div>
+              )}
 
-            {/* RESPONDERS SELECTION */}
-            {isAddMode && (
-              <div className="border bg-white absolute left-0 max-h-40 overflow-x-auto flex flex-col gap-2">
-                {results.isLoading ? (
-                  <p>Loading Responders</p>
-                ) : (
-                  results.data?.unassignedResponders.map((responder) => (
-                    <div
-                      key={responder._id}
-                      className="border p-1 hover:bg-gray-300 cursor-pointer"
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        setSelectedHead(responder);
-                        setIsAddMode(false);
-                      }}
-                    >
-                      <span>{responder._id}</span>
-                    </div>
-                  ))
-                )}
-              </div>
-            )}
-          </>
-        )}
+              {/* RESPONDERS SELECTION */}
+              {isAddMode && (
+                <div className="border bg-white absolute left-0 max-h-40 overflow-x-auto flex flex-col gap-2">
+                  {results.isLoading ? (
+                    <p>Loading Responders</p>
+                  ) : (
+                    results.data?.unassignedResponders.map((responder) => (
+                      <div
+                        key={responder._id}
+                        className="border p-1 hover:bg-gray-300 cursor-pointer"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          setSelectedHead(responder);
+                          setIsAddMode(false);
+                        }}
+                      >
+                        <span>{responder._id}</span>
+                      </div>
+                    ))
+                  )}
+                </div>
+              )}
+            </>
+          )}
+        </div>
+        <button onClick={() => setShowModal(true)}>Hello</button>
+
+        {/* Team Members */}
+        <MembersTable membersData={teamData?.members || []} />
       </div>
-      {/* Team Members */}
-      <MembersTable membersData={teamData?.members || []} />
-      {/* {teamData?.members && teamData.members.length > 0 ? (
-          <div>
-            <span>Team Members</span>
-            <div className="">
-              {teamData.members.map((member) => (
-                <UserCard key={member._id} user={member} />
-              ))}
-            </div>
-          </div>
-        ) : (
-          <></>
-        )} */}
-    </div>
+      <Modal
+        modalTitle={"Edit Team"}
+        modalShow={showModal}
+        modalClose={() => setShowModal(false)}
+      >
+        <span> Hello World</span>
+      </Modal>
+    </>
   );
 };
 
