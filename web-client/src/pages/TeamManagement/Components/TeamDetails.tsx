dd3683208f104288955e61931fb64f47e62bfc63
Clarence Joseph Dimafelix <clarencejoseph.dimafelix@gmail.com>
2023-07-31 12:32:18 +0800

    refactor: team details styling and layout

--- web-client/src/pages/TeamManagement/Components/TeamDetails.tsx
+++ web-client/src/pages/TeamManagement/Components/TeamDetails.tsx
@@ -33,40 +33,39 @@ const TeamDetails = () => {
   return (
     <>
       <div className="p-5 flex-grow">
-        <div>
-          <span className="text-sm text-gray-400">{teamData?._id}</span>
-          <h1 className="text-3xl font-bold">{teamData?.name}</h1>
+        <div className="flex flex-row justify-between items-center mb-10">
+          <div>
+            <span className="text-sm text-gray-400">{teamData?._id}</span>
+            <h1 className="text-3xl font-bold">{teamData?.name}</h1>
+          </div>
+          <button
+            className="bg-indigo-500 text-white px-6 py-2 rounded mt-2"
+            onClick={() => {
+              getResponders();
+              setShowModal(true);
+            }}
+          >
+            Edit Team
+          </button>
         </div>
         {/* Team Leader */}
         <div className="relative">
-          <span>Team Leader: </span>
+          <span className="font-semibold text-lg">Team Leader </span>
           {teamData && teamData.head !== null ? (
             <UserCard user={teamData.head} />
           ) : (
-            <>
-              <div className="border px-3 py-1 cursor-pointer rounded-md relative">
-                <p className="text-gray-700 text-[16px] leading-tight">
-                  No leader assigned for this team.
-                </p>
-                <span className="text-sm text-gray-500">
-                  Click here to assign.
-                </span>
-              </div>
-            </>
+            <div className="my-2">
+              <p className="text-gray-700 text-[16px] leading-tight">
+                No leader assigned for this team.
+              </p>
+            </div>
           )}
         </div>
-        <button
-          className="bg-indigo-500 text-white px-6 py-2 rounded mt-2"
-          onClick={() => {
-            getResponders();
-            setShowModal(true);
-          }}
-        >
-          Edit Team
-        </button>
-
-        {/* Team Members */}
-        <MembersTable membersData={teamData?.members || []} />
+        <div className="">
+          {/* Team Members */}
+          <span className="font-semibold text-lg">Team Members </span>
+          <MembersTable membersData={teamData?.members || []} />
+        </div>
       </div>
       <Modal
         modalTitle={"Edit Team"}
