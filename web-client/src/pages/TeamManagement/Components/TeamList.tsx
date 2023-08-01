c5fdc50c9fe2a56a49b06396585f6d20369abbc5
Clarence Joseph Dimafelix <clarencejoseph.dimafelix@gmail.com>
2023-08-01 16:22:27 +0800

    fix: add team loading skeleton appearing on idle

--- web-client/src/pages/TeamManagement/Components/TeamList.tsx
+++ web-client/src/pages/TeamManagement/Components/TeamList.tsx
@@ -38,7 +38,7 @@ const TeamList = () => {
                 <p className="text-lg">{team.name}</p>
               </div>
             ))}
-            {!createTeamState.isLoading && (
+            {createTeamState.isLoading && (
               <div className="animate-pulse w-full h-16 my-2 p-2 rounded-md bg-gray-200">
                 <div className="w-32 h-3 bg-gray-300 rounded-full"></div>
                 <div className="w-full h-6 mt-2 bg-gray-300 rounded-md"></div>
