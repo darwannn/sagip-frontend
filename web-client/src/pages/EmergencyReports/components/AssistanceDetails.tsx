eeda8a6d8cf0e9dff5766231b4ac25817d19fbc7
Clarence Joseph Dimafelix <clarencejoseph.dimafelix@gmail.com>
2023-08-19 13:16:03 +0800

    add: display additional emergency details

--- web-client/src/pages/EmergencyReports/components/AssistanceDetails.tsx
+++ web-client/src/pages/EmergencyReports/components/AssistanceDetails.tsx
@@ -9,56 +9,70 @@ const AssistanceDetails = () => {
   console.log(assistanceReq);
 
   return (
-    <div className="border rounded-md shadow-sm p-2 mx-2  bg-white z-10 fixed right-0 top-[50%] translate-y-[-50%] min-w-[500px]">
-      <span>{assistanceReq?._id}</span>
-      {/* USER DETAILS */}
-      <div className="user-info flex flex-row items-center gap-2 p-2">
-        <div className="pic-container">
-          <img
-            src={`${BASE_IMAGE_URL}/user/${assistanceReq?.userId.profilePicture}`}
-            alt="user"
-            className="rounded-full w-20 h-20"
-          />
-        </div>
-        <div className="user-details grid grid-cols-2 gap-2">
-          <div>
-            <span className="text-sm text-gray-500">Name:</span>
-            <p className="font-semibold">{`${assistanceReq?.userId.firstname} ${assistanceReq?.userId.lastname}`}</p>
+    <div className="border rounded-md shadow-sm p-2 mx-2 h-[80vh]  bg-white z-10 fixed right-0 top-[50%] translate-y-[-50%] min-w-[500px]">
+      <div className="">
+        <span>{assistanceReq?._id}</span>
+        {/* USER DETAILS */}
+        <div className="user-info flex flex-row items-center gap-2 p-2 my-2">
+          <div className="pic-container">
+            <img
+              src={`${BASE_IMAGE_URL}/user/${assistanceReq?.userId.profilePicture}`}
+              alt="user"
+              className="rounded-full w-20 h-20"
+            />
           </div>
-          <div>
-            <span className="text-sm text-gray-500">Phone:</span>
-            <p className="font-semibold">
-              {assistanceReq?.userId.contactNumber}
-            </p>
-          </div>
-          <div className="col-span-2">
-            <span className="text-sm text-gray-500">Address:</span>
-            <p>{`${assistanceReq?.userId.street} ${assistanceReq?.street} ${assistanceReq?.userId.municipality} ${assistanceReq?.userId.province}`}</p>
-          </div>
-        </div>
-      </div>
-      {/* EMERGENCY / ASSISTANCE DETAILS */}
-      <div className="emergency-details bg-red-100 p-2 rounded-md">
-        <div className="flex flex-row items-center gap-3">
-          <div className="emergency-icon">
-            <div className="bg-red-300 w-20 h-20 rounded-full"></div>
-          </div>
-          <div className="emergency-details grid grid-cols-2 gap-3">
-            <div className="col-span-2">
-              <h3 className="font-bold text-xl text-red-500">Trapped</h3>
+          <div className="user-details grid grid-cols-2 gap-2">
+            <div>
+              <span className="text-sm text-gray-500">Name:</span>
+              <p className="font-semibold">{`${assistanceReq?.userId.firstname} ${assistanceReq?.userId.lastname}`}</p>
             </div>
-            <div className="">
-              <span className="text-sm text-gray-500">Time Reported</span>
-              <p className="font-bold">
-                {moment(assistanceReq?.createdAt).format("HH:mm A")}
+            <div>
+              <span className="text-sm text-gray-500">Phone:</span>
+              <p className="font-semibold">
+                {assistanceReq?.userId.contactNumber}
               </p>
             </div>
-            <div className="">
-              <span className="text-sm text-gray-500">Location</span>
-              <p className="font-bold">{assistanceReq?.street}</p>
+            <div className="col-span-2">
+              <span className="text-sm text-gray-500">Address:</span>
+              <p>{`${assistanceReq?.userId.street} ${assistanceReq?.street} ${assistanceReq?.userId.municipality} ${assistanceReq?.userId.province}`}</p>
             </div>
           </div>
-          <div></div>
+        </div>
+        {/* EMERGENCY / ASSISTANCE DETAILS */}
+        <div className="emergency-details bg-red-100 p-2 my-2 rounded-md">
+          <div className="flex flex-row items-center gap-3">
+            <div className="emergency-icon">
+              <div className="bg-red-300 w-20 h-20 rounded-full"></div>
+            </div>
+            <div className="emergency-details grid grid-cols-2 gap-3">
+              <div className="col-span-2">
+                <h3 className="font-bold text-xl text-red-500">Trapped</h3>
+              </div>
+              <div className="">
+                <span className="text-sm text-gray-500">Time Reported</span>
+                <p className="font-bold">
+                  {moment(assistanceReq?.createdAt).format("HH:mm A")}
+                </p>
+              </div>
+              <div className="">
+                <span className="text-sm text-gray-500">Location</span>
+                <p className="font-bold">{assistanceReq?.street}</p>
+              </div>
+            </div>
+            <div></div>
+          </div>
+        </div>
+        {/* ADDITIONAL DETAILS */}
+        <div className="additional-details bg-blue-200 p-2">
+          <span className="text-sm text-gray-500">Additional Details</span>
+          <div className="image-container max-h-[200px] overflow-hidden">
+            <img
+              src={`${BASE_IMAGE_URL}/assistance-request/${assistanceReq?.proof}`}
+              alt="proof image"
+              className="w-[200px] object-cover"
+            />
+          </div>
+          <p>{assistanceReq?.description}</p>
         </div>
       </div>
     </div>
