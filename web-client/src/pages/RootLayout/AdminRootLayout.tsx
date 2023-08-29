20f5fba2efe92312ee2a83b1be4e3330cb3abb59
Clarence Joseph Dimafelix <clarencejoseph.dimafelix@gmail.com>
2023-08-29 21:07:47 +0800

    refactor: old sidebar cleanup

--- web-client/src/pages/RootLayout/AdminRootLayout.tsx
+++ web-client/src/pages/RootLayout/AdminRootLayout.tsx
@@ -1,52 +1,10 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import AdminSidebar from "../../components/Navigation/AdminSidebar";
 
 const AdminRootLayout = () => {
   return (
     <div className="fixed root flex flex-row h-screen w-screen">
       <AdminSidebar />
-      {/* <div className="sidebar flex flex-col w-60">
-        <nav>
-          <ul>
-            <li>
-              <NavLink to={"/"}>Dashboard</NavLink>
-            </li>
-            <li>
-              <NavLink to={"/users"}>Users</NavLink>
-            </li>
-            <li>
-              <NavLink to={"/teams"}>Teams</NavLink>
-            </li>
-            <li>
-              <NavLink to={"/emergency-reports"}>Emergency Reports</NavLink>
-            </li>
-            <li>
-              <NavLink to={"/hazard-reports"}>Hazard Reports</NavLink>
-            </li>
-            <li>
-              <NavLink to={"/facility-map"}>Facility Map</NavLink>
-            </li>
-            <li>
-              <NavLink to={"/disaster-alerts"}>Disaster Alerts</NavLink>
-            </li>
-            <li>
-              <NavLink to={"/articles"}>Articles</NavLink>
-            </li>
-            <li>
-              <NavLink to={"/account-settings"}>Account Settings</NavLink>
-            </li>
-            <li>
-            <NavLink to={"/login"}>Login</NavLink>
-          </li>
-          <li>
-            <NavLink to={"/register"}>Register</NavLink>
-          </li>
-          <li>
-          <NavLink to={"/forgot-password"}>Forgot Password</NavLink>
-          </li>
-          </ul>
-        </nav>
-      </div> */}
       <div className="outlet grow overflow-y-auto">
         <Outlet />
       </div>
