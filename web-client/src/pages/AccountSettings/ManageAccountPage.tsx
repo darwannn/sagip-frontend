0a0bdbbfaff2d21787562deb8ca73debd7891297
Clarence Joseph Dimafelix <clarencejoseph.dimafelix@gmail.com>
2023-09-01 21:10:51 +0800

    refactor: admin profile settings removed mobile

--- web-client/src/pages/AccountSettings/ManageAccountPage.tsx
+++ web-client/src/pages/AccountSettings/ManageAccountPage.tsx
@@ -1,29 +1,11 @@
-import { useEffect, useState } from "react";
-import { Outlet, useParams } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import { useGetUserByTokenQuery } from "../../services/accountQuery";
 
 import AccountSettingsList from "./components/AccountSettingsList";
-import ViewAccountPage from "./components/ViewAccountPage";
 
 const ManageAccountPage = () => {
-  const { page } = useParams();
-
-  const [isMobile, setIsMobile] = useState(false);
   const { data: userData, isLoading, error } = useGetUserByTokenQuery();
-
-  /* checks if the screen size is less than 768px (xs/default to md breakpoint) */
-  const checkScreenSize = () => {
-    const isMobileSize = window.matchMedia("(max-width: 768px)").matches;
-    setIsMobile(isMobileSize);
-  };
-  useEffect(() => {
-    checkScreenSize();
-    window.addEventListener("resize", checkScreenSize);
-    return () => {
-      window.removeEventListener("resize", checkScreenSize);
-    };
-  }, [isMobile]);
 
   if (isLoading) {
     return <p>Loading...</p>;
@@ -34,29 +16,14 @@ const ManageAccountPage = () => {
   }
 
   return (
-    <>
-      {isMobile ? (
-        /* for mobile, show only one component at a time */
-        <div className="flex flex-col h-full">
-          {page !== "profile" &&
-            page !== "email" &&
-            page !== "contact-number" &&
-            page !== "password" && <AccountSettingsList userData={userData} />}
-
-          {page && <ViewAccountPage userData={userData} />}
-        </div>
-      ) : (
-        /* for web, show two components at the same time */
-        <div className="w-full flex">
-          <AccountSettingsList userData={userData} />
-          {/* flex-1, takes the rest of the space */}
-          {/* <ViewAccountPage userData={userData} /> */}
-          <div className="flex-1">
-            <Outlet />
-          </div>
-        </div>
-      )}
-    </>
+    <div className="w-full flex">
+      <AccountSettingsList userData={userData} />
+      {/* flex-1, takes the rest of the space */}
+      {/* <ViewAccountPage userData={userData} /> */}
+      <div className="flex-1">
+        <Outlet />
+      </div>
+    </div>
   );
 };
 
