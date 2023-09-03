99880efebe28c7b6d757e9483b4320db7bce17da
Clarence Joseph Dimafelix <clarencejoseph.dimafelix@gmail.com>
2023-09-03 11:38:30 +0800

    add: SMSTemplateItem component

---
+++ web-client/src/pages/Admin/AlertsManagement/components/SMSTemplateItem.tsx
@@ -0,0 +1,16 @@
+const SMSTemplateItem = () => {
+  return (
+    <div className="template-item p-5 h-[150px] bg-gray-100 shadow-sm rounded-md">
+      <div className="template-title mb-2">
+        <h5 className="text-lg font-semibold text-gray-700">Template Title</h5>
+      </div>
+      <div className="template-message">
+        <span className="line-clamp-3 text-gray-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, dignissimos adipisci? Sequi, nobis deleniti voluptatum consequuntur, qui quis dolor nisi eveniet ipsam quam quasi perspiciatis dicta. Vero consequuntur modi earum aperiam delectus officiis temporibus praesentium excepturi in quas porro amet ut mollitia voluptatem eos quam sunt asperiores perferendis cupiditate consectetur sequi, sapiente totam est alias. Facere adipisci, a error possimus quam blanditiis repudiandae dicta dignissimos dolorem vero! Tempore corrupti similique aspernatur eveniet aut, harum dolorum a? Dicta, illo quas exercitationem minus voluptatem nobis et facere unde mollitia nam? Tempora illo incidunt deleniti quam molestias officia excepturi tenetur et at? Nobis suscipit, animi illum enim id adipisci illo nulla itaque obcaecati nemo veritatis accusantium natus dignissimos explicabo neque maxime facilis mollitia ratione saepe dolores optio doloremque dolorem voluptatum? Tempora excepturi provident, similique corporis maxime alias nobis accusantium! Dolor dolorem odio fuga, facilis aspernatur fugit explicabo repellendus. Rerum, officiis animi quam laboriosam non consequatur voluptas iste earum doloremque, sequi distinctio tempore blanditiis? Officia optio suscipit earum praesentium repellendus nisi qui cum saepe vel, facilis maxime deleniti, eveniet neque, quia quibusdam eligendi. Eos magnam aliquid ratione unde, atque ipsam optio! Sit, ipsum! Ut similique perspiciatis cupiditate aut labore doloremque quia velit neque nesciunt!
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default SMSTemplateItem;
