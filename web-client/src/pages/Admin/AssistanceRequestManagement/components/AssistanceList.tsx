89d2332fb24a0b3f7ba749fec05434e399c99f59
Clarence Joseph Dimafelix <clarencejoseph.dimafelix@gmail.com>
2023-09-26 15:33:36 +0800

    refactor: assistance request filtering

--- web-client/src/pages/Admin/AssistanceRequestManagement/components/AssistanceList.tsx
+++ web-client/src/pages/Admin/AssistanceRequestManagement/components/AssistanceList.tsx
@@ -1,26 +1,26 @@
-import { memo } from "react";
-import { useGetAllAssistanceRequestsQuery } from "../../../../services/assistanceRequestQuery";
+import React, { memo } from "react";
 import AssistanceItem from "./AssistanceItem";
-import { useSearchParams } from "react-router-dom";
-const AssistanceList = memo(() => {
-  const { data } = useGetAllAssistanceRequestsQuery();
-  const [searchParams] = useSearchParams();
-  const filter = searchParams.get("filter") || "unverified";
+import { TAssistanceRequest } from "../../../../types/assistanceRequest";
 
-  const filteredAssistance = data?.filter(
-    (assistance) => assistance.status === filter
-  );
+type AssistanceListProps = {
+  assistanceRequests: TAssistanceRequest[];
+};
 
-  return (
-    <div className="flex-grow flex flex-col gap-5 px-1 w-full overflow-y-auto">
-      {filteredAssistance?.map((assistance) => (
-        <AssistanceItem key={assistance._id} assistance={assistance} />
-      ))}
-      {filteredAssistance?.length === 0 && (
-        <div className="text-center p-5 text-gray-500">No Assistance Found</div>
-      )}
-    </div>
-  );
-});
+const AssistanceList: React.FC<AssistanceListProps> = memo(
+  ({ assistanceRequests }) => {
+    return (
+      <div className="flex-1 flex flex-col gap-5 px-1 w-full overflow-y-auto">
+        {assistanceRequests?.map((assistance) => (
+          <AssistanceItem key={assistance._id} assistance={assistance} />
+        ))}
+        {assistanceRequests?.length === 0 && (
+          <div className="text-center p-5 text-gray-500">
+            No Assistance Found
+          </div>
+        )}
+      </div>
+    );
+  }
+);
 
 export default AssistanceList;
