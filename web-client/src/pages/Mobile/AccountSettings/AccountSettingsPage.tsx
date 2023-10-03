8d98243c83cbc74bbb18ae5060deba6093ec0acc
Clarence Joseph Dimafelix <clarencejoseph.dimafelix@gmail.com>
2023-10-03 22:39:42 +0800

    refactor: account settings header contents

--- web-client/src/pages/Mobile/AccountSettings/AccountSettingsPage.tsx
+++ web-client/src/pages/Mobile/AccountSettings/AccountSettingsPage.tsx
@@ -8,16 +8,20 @@ const AccountSettingsPage = () => {
   const { data: user /* isLoading, error */ } = useGetUserByTokenQuery();
   return (
     <div className="min-h-screen mx-auto">
-      <MobileHeader
-        component={
-          <>
+      <MobileHeader>
+        <div className="flex flex-col items-center justify-center w-full gap-2">
+          <div>
             <img
               src={`${BASE_IMAGE_URL}/user/${user?.profilePicture}`}
-              className="rounded-full w-20 m-auto"
+              className="rounded-full w-16 m-auto outline outline-4 outline-white"
             />
-          </>
-        }
-      />
+          </div>
+          <div className="text-sm text-center">
+            <p className="font-semibold">{`${user?.firstname} ${user?.lastname}`}</p>
+            <p className="">{user?.email}</p>
+          </div>
+        </div>
+      </MobileHeader>
       <AccountSettingsList />
     </div>
   );
