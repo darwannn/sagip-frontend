e2e4ecf6eb119790a736170025a977583d4cb8c7
Clarence Joseph Dimafelix <clarencejoseph.dimafelix@gmail.com>
2023-10-04 10:15:23 +0800

    refactor: settings page header

--- web-client/src/pages/Mobile/AccountSettings/AccountSettingsPage.tsx
+++ web-client/src/pages/Mobile/AccountSettings/AccountSettingsPage.tsx
@@ -3,22 +3,36 @@ import { BASE_IMAGE_URL } from "../../../api.config";
 
 import MobileHeader from "../../../components/MobileHeader/MobileHeader";
 import AccountSettingsList from "../../Mobile/AccountSettings/components/AccountSettingsList";
+import { LuArrowLeft } from "react-icons/lu";
+import { useNavigate } from "react-router-dom";
 
 const AccountSettingsPage = () => {
   const { data: user /* isLoading, error */ } = useGetUserByTokenQuery();
+  const navigation = useNavigate();
   return (
     <div className="min-h-screen mx-auto">
       <MobileHeader>
-        <div className="flex flex-col items-center justify-center w-full gap-2">
-          <div>
-            <img
-              src={`${BASE_IMAGE_URL}/user/${user?.profilePicture}`}
-              className="rounded-full w-16 m-auto outline outline-4 outline-white"
-            />
+        <div className="w-full">
+          <div className="mb-3 flex items-center gap-3">
+            <button
+              className="p-1.5 bg-white text-gray-500 rounded-md"
+              onClick={() => navigation(-1)}
+            >
+              <LuArrowLeft />
+            </button>
+            <p className="font-semibold">Settings</p>
           </div>
-          <div className="text-sm text-center">
-            <p className="font-semibold">{`${user?.firstname} ${user?.lastname}`}</p>
-            <p className="">{user?.email}</p>
+          <div className="flex flex-row items-center justify-between w-full gap-2">
+            <div className="text-sm">
+              <p className="font-medium">{`${user?.firstname} ${user?.lastname}`}</p>
+              <p className="">{user?.email}</p>
+            </div>
+            <div>
+              <img
+                src={`${BASE_IMAGE_URL}/user/${user?.profilePicture}`}
+                className="rounded-full w-16 m-auto "
+              />
+            </div>
           </div>
         </div>
       </MobileHeader>
